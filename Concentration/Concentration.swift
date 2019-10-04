94ac25f1ddf23b94785dda793311df2db160c164
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-10-04 15:11:15 +0400

    chnage oneAndOnlyIndex instacne variable for concentration class to computable

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,7 +12,26 @@ class Concentration {
     
     var cards = Array<Card>()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var cardIndex: Int?
+            for flipDownIndex in cards.indices {
+                if cards[flipDownIndex].isFaceUp {
+                    if cardIndex == nil {
+                        cardIndex = flipDownIndex;
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return cardIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     init(numberOfPiarsOfCards: Int) {
         for _ in 1...numberOfPiarsOfCards {
@@ -29,15 +48,9 @@ class Concentration {
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
-                    
                 }
                 cards[index].isFaceUp = true;
-                indexOfOneAndOnlyFaceUpCard = nil;
             } else {
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
